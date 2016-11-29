8fc8c376fb7d58afb7b4d2df0e7a06a005d574f4
skoobgeneral <jsdelvastoa@gmail.com>
2016-11-29 11:39:16 -0500

    fixing child theme read

--- functions.php
+++ functions.php
@@ -4,3 +4,5 @@
 * Add your own functions here. You can also copy some of the theme functions into this file. 
 * Wordpress will use those functions instead of the original functions then.
 */
+
+require_once( get_stylesheet_directory() . '/css/custom.css' );
