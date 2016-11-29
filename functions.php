7850b9683d513730971aeaf21062938505b1ec35
skoobgeneral <jsdelvastoa@gmail.com>
2016-11-29 11:41:57 -0500

    fixing child theme read

--- functions.php
+++ functions.php
@@ -5,4 +5,14 @@
 * Wordpress will use those functions instead of the original functions then.
 */
 
-require_once( get_stylesheet_directory() . '/css/custom.css' );
+add_action( 'wp_enqueue_scripts', 'wp_enqueue_scripts_mod', 20 );
+
+function wp_enqueue_scripts_mod() {
+    
+    wp_dequeue_style( 'avia-child' );
+    wp_deregister_style( 'avia-child' );
+
+    wp_register_style( 'new-child-style', get_stylesheet_directory_uri() . '/new.css', false, '1.0.0' ); 
+    wp_enqueue_style( 'new-child-style' );
+
+}
