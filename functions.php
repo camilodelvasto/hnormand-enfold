a85bcf478cd49ea8a7e572b4d55a885c2c941ee5
skoobgeneral <jsdelvastoa@gmail.com>
2016-11-29 11:50:27 -0500

    remove some functions

--- functions.php
+++ functions.php
@@ -4,15 +4,3 @@
 * Add your own functions here. You can also copy some of the theme functions into this file. 
 * Wordpress will use those functions instead of the original functions then.
 */
-
-add_action( 'wp_enqueue_scripts', 'wp_enqueue_scripts_mod', 20 );
-
-function wp_enqueue_scripts_mod() {
-    
-    wp_dequeue_style( 'avia-child' );
-    wp_deregister_style( 'avia-child' );
-
-    wp_register_style( 'new-child-style', get_stylesheet_directory_uri() . '/style.css', false, '2' ); 
-    wp_enqueue_style( 'new-child-style' );
-
-}
